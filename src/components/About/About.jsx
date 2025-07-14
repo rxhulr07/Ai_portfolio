954daaddc30368b55b570814097d4156f3e3bb5a
Rahulranjan070 <rahulranjan1245@gmail.com>
2025-07-15 02:22:30 +0530

    fixed error while deployemnt on aws s3 bucket

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
-// import profileImage from '../../assets/profile2.png';
+
 
 const About = () => {
   return (
@@ -73,7 +73,7 @@ const About = () => {
             gyroscope={true}
           >
             <img
-              src={profileImage}
+              // src={profileImage}
               alt="Rahul ranjan"
               className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
             />
