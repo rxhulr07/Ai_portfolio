66de5182057141524c51d29c7ee79f9f48b67e9f
Rahulranjan070 <rahulranjan1245@gmail.com>
2025-07-15 02:38:43 +0530

    dist file updated

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { education } from "../../constants"; // Import the education data
 
 const Education = () => {
--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { experiences } from "../../constants"; // Import your data
 
 const Experience = () => {
